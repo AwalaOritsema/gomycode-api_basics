05d44009bec2cf3993fa30a3f8ce84aba06531ae
Michael Peter <michaelhpet@gmail.com>
2023-06-01 21:10:10 +0100

    feat: cleanup styles

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,20 @@
 export default function User(props) {
   return (
     <article className="user_card">
-      <p>{props.name}</p>
-      <p>{props.username}</p>
-      <p>{props.email}</p>
-      <p>{props.phone}</p>
       <p>
+        <span>Name</span> {props.name}
+      </p>
+      <p>
+        <span>Username</span> {props.username}
+      </p>
+      <p>
+        <span>Email</span> {props.email}
+      </p>
+      <p>
+        <span>Phone</span> {props.phone}
+      </p>
+      <p>
+        <span>Address</span>
         {props.address.street} {props.address.suite} {props.address.city}
       </p>
     </article>
